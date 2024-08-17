4ff6b126540f9ac60f913a64b59d4214028a99f1
Keaphs Nguyen <keaphsn@gmail.com>
2024-08-17 02:42:58 +0000

    finished registration and login tests

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,14 +1,29 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import javafx.scene.shape.Mesh;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.util.List;
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController(){
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,7 +31,8 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::postAccountHandler);
+        app.post("/login", this::getLoginHandler);
 
         return app;
     }
@@ -25,8 +41,33 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    private void postAccountHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+        if(addedAccount!=null){
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }else{
+            ctx.status(400);
+        }
+    }
+
+    private void getLoginHandler(Context ctx) throws JsonProcessingException{
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Account account = mapper.readValue(ctx.body(), Account.class);
+            Account loginAccount = accountService.loginAccount(account);
+            if(loginAccount != null){
+                ctx.json(loginAccount);
+                ctx.status(200);
+            }
+            else {
+                ctx.status(401);
+            }
+        }catch (JsonProcessingException e) {
+            ctx.status(400); 
+        }
+        
     }
 
 
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,37 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+
+import java.util.List;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+
+    public List<Account> getAllAccounts(){
+        return accountDAO.getAllAccounts();
+    }
+
+    public Account addAccount(Account account){
+        Account persistentAccount = new Account();
+        if(account.getPassword().length() > 4 && account.getUsername().length() > 1){
+            persistentAccount = accountDAO.insertAccount(account);
+            return persistentAccount;
+        }
+        else
+        return null;              
+    }
+
+    public Account loginAccount(Account account){
+        Account login = accountDAO.getAccount(account.getUsername(), account.getPassword());
+        return login;
+    }
+}
